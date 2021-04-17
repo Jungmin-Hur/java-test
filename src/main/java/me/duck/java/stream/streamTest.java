5c12bc5a97e9a61645a142945821c4f63c8b543a
duck <jungmin5.hur@gmail.com>
2021-04-18 00:06:11 +0900

    stream sample

---
+++ src/main/java/me/duck/java/stream/streamTest.java
@@ -0,0 +1,35 @@
+package me.duck.java.stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class streamTest {
+    public static void main(String args[]) {
+        List<List<String>> list = new ArrayList<>();
+        list.add(Arrays.asList("a1","b1"));
+        list.add(Arrays.asList("a2","b2"));
+        list.add(Arrays.asList("a3","b3"));
+        list.add(Arrays.asList("a4","b4"));
+        list.add(Arrays.asList("a5","b5"));
+
+        // 두코드의 결과가 같음
+        extractStringList(list);
+        extractStringListUsingStream(list);
+    }
+
+    public static void extractStringList(List<List<String>> list) {
+        List<List<String>> subList = list.subList(2, 4);
+        List<String> res = new ArrayList<>();
+        for (List<String> item : subList) {
+            res.add(item.get(0));
+        }
+        System.out.println(res);
+    }
+
+    public static void extractStringListUsingStream(List<List<String>> list) {
+        List<String> res = list.subList(2, 4).stream().map(item -> item.get(0)).collect(Collectors.toList());
+        System.out.println(res);
+    }
+}
