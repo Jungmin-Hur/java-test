45cd74ebea1501ba98a32bda23edbeba58d30205
duck <jungmin5.hur@gmail.com>
2021-05-30 12:59:00 +0900

    .

--- src/main/java/me/duck/java/stream/streamTest.java
+++ src/main/java/me/duck/java/stream/streamTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class streamTest {
+
     public static void main(String args[]) {
 
         Arrays.stream(new String[]{"c", "python", "java"})
